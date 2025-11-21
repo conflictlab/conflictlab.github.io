ba491ca296861bcfbef4a9b00661ebdca8c6bc55
chadefa1 <chadefaux@gmail.com>
2025-11-21 11:57:49 +0000

    fix(publications): import Link for quick filter/acknowledgements links

--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useMemo } from 'react'
 import { useSearchParams } from 'next/navigation'
+import Link from 'next/link'
 import { Search, X } from 'lucide-react'
 import Breadcrumbs from '@/components/Breadcrumbs'
 
