75c1e34f065031634d5a421f6dd444231cbec553
chadefa1 <chadefaux@gmail.com>
2025-11-24 22:17:15 +0000

    Remove no-cache meta from head; caching should be handled via headers/CDN

--- app/head.tsx
+++ app/head.tsx
@@ -1,10 +1,4 @@
 export default function Head() {
-  return (
-    <>
-      <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-      <meta httpEquiv="Pragma" content="no-cache" />
-      <meta httpEquiv="Expires" content="0" />
-    </>
-  )
+  // Intentionally left minimal. Cache control should be managed at the CDN/server layer.
+  return <></>
 }
-
