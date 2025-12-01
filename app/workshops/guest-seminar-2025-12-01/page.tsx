5b7867366dd2863a59f8d9ffff841b99658bba90
chadefa1 <chadefaux@gmail.com>
2025-12-01 10:14:00 +0000

    Update seminar details formatting in page.tsx

--- app/workshops/guest-seminar-2025-12-01/page.tsx
+++ app/workshops/guest-seminar-2025-12-01/page.tsx
@@ -13,7 +13,7 @@ export default function GuestSeminar2025Dec01() {
           </p>
           <p className="text-sm text-gray-600 mb-2">2025</p>
           <h1 className="text-2xl md:text-3xl font-light text-gray-900 mb-3 leading-snug">
-            Guest seminar, Monday 1 december 2025, Dr Simon Polichinel von der Maase, Senior Researcher - peace research institute Oslo (PRIO).
+            Guest seminar, Monday 1 december 2025 \n Dr Simon Polichinel von der Maase, Senior Researcher - Peace Research Institute Oslo (PRIO).
           </h1>
           <p className="text-lg text-gray-700">
             <span className="italic"></span>
